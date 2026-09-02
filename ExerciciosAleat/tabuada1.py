ee8c33b53358e3eb6ccb437756c154339e57566f
Pedro Oliveira Pires <2601754@SALAS.AULAS>
2026-09-02 20:40:43 -0300

    exercicio aula

--- ExerciciosAleat/tabuada1.py
+++ ExerciciosAleat/tabuada1.py
@@ -1,6 +1,18 @@
+"""
 t = int(input("Digite um número para ver a tabuada: "))
 cont = 0
 
 while cont != 10:
     cont += 1
     print(f'{cont} x {t} = {cont * t}'  )
+"""
+
+t = int(input("Digite um número para ver a tabuada: "))
+cont = 0
+cont2 = 11
+
+while cont != 10 and cont2 != 1:
+    cont += 1
+    print(f'{cont} x {t} = {cont * t}')
+    cont2 -= 1
+    print(f'{cont2} x {t} = {cont2 * t}')      
